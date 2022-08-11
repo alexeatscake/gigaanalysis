747c38db396088fe61a420958916aabf80ccccde
Alex Hickey <alexander.john.hickey@gmail.com>
2022-08-11 15:30:18 +0100

    🪀 Fixed incorrect date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'gigaanalysis'
-copyright = '2020, Alex Hickey'
+copyright = '2022, Alex Hickey'
 author = 'Alex Hickey'
 
 # The full version, including alpha/beta/rc tags
